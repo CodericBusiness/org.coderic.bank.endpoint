44cbbeb422f6d71be87d5c08ffcaa02ba4a88fbd
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-02-26 15:32:26 -0500

    Refactory

--- src/main/java/org/coderic/bank/endpoint/config/WebServiceConfig.java
+++ src/main/java/org/coderic/bank/endpoint/config/WebServiceConfig.java
@@ -29,7 +29,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("CountriesPort");
         wsdl11Definition.setLocationUri("/ws");
-        wsdl11Definition.setTargetNamespace("http://coderic.org/guides/gs-producing-web-service");
+        wsdl11Definition.setTargetNamespace("http://coderic.org/bank/ws");
         wsdl11Definition.setSchema(countriesSchema);
         return wsdl11Definition;
     }
